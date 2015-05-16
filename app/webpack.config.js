e9a3265b544a2b93862ae816eb9d1b6007760798
Boris Joffe <boris@joff3.com>
2015-05-16 01:23:16 -0700

    start adding webpack with babel loader/runtime

---
+++ app/webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	loaders: [
+		// the optional 'runtime' transformer tells babel to require the runtime
+		// instead of inlining it.
+		{
+			test: /\.es6?$/,
+			exclude: /(node_modules|bower_components)/,
+			loader: 'babel-loader?optional[]=runtime'
+		}
+	]
+};
