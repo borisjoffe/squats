11ac42a97e12990357e7922c7e1ab92233a5c50e
Boris Joffe <boris@joff3.com>
2015-05-17 03:02:59 -0700

    add webpack entry

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-	loaders: [
-		// the optional 'runtime' transformer tells babel to require the runtime
-		// instead of inlining it.
-		{
-			test: /\.es6?$/,
-			exclude: /(node_modules|bower_components)/,
-			loader: 'babel-loader?optional[]=runtime'
-		}
-	]
+	entry: './pub/app.js',
+	module: {
+		loaders: [
+			// the optional 'runtime' transformer tells babel to require the runtime
+			// instead of inlining it.
+			{
+				test: /\.es6?$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: 'babel-loader?optional[]=runtime'
+			}
+		]
+	}
 };
