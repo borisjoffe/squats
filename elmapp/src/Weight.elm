0856d75e889837dc023c011854d651a973ae6019
Boris Joffe <boris@joff3.com>
2016-05-26 15:49:40 -0700

    cleanup

--- elmapp/src/Weight.elm
+++ elmapp/src/Weight.elm
@@ -1,34 +1,37 @@
 module Weight exposing
     (..)
-    -- (Weight, WeightUnit, weightLbs, WeightValue)
-    -- (Weight, WeightUnit(Kg,Lbs), weightLbs, WeightValue(Float,Unknown))
 
+-- Model
 type WeightValue v = Float v | Unknown
 
 type WeightUnit = Kg | Lbs
-
 
 type alias Weight w =
     { units : WeightUnit
     , value : WeightValue w
     }
 
+-- Curried constructors
+weightLbs : WeightValue w -> Weight w
+weightLbs = Weight Lbs
 
-printValue : Weight x -> String
+weightKg : WeightValue w -> Weight w
+weightKg = Weight Kg
+
+
+-- View
+printValue : Weight w -> String
 printValue w =
     case .value w of
         Float x -> toString x
         Unknown -> "unknown"
 
-printUnits : Weight x -> String
+printUnits : Weight w -> String
 printUnits w =
     case .units w of
         Kg -> "kg"
         Lbs -> "lbs"
 
-print : Weight x -> String
+print : Weight w -> String
 print w =
     printValue w ++ " " ++ printUnits w
-
-weightLbs : WeightValue a -> Weight a
-weightLbs = Weight Lbs
